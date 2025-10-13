2a6b4668c44fa1b5402a9dfc1bf551d387a915b1
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-13 19:07:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5456750555-b2544",
+  "appId": "1:874834659348:web:a71628b2120dd2523dbf01",
+  "apiKey": "AIzaSyA1Vrx2X0fgnTahBkv3rhgKFjzcs6hViT8",
+  "authDomain": "studio-5456750555-b2544.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "874834659348"
+};
