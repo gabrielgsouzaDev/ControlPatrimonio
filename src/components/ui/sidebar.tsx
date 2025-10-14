6ae51748c00a365ced5619390f690b67c24e4b48
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-14 02:48:33 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
 import { PanelLeft } from "lucide-react"
+import * as SheetPrimitive from "@radix-ui/react-dialog"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -206,6 +207,10 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <SheetPrimitive.Title className="sr-only">Menu</SheetPrimitive.Title>
+            <SheetPrimitive.Description className="sr-only">
+              Navegue pelas seções do aplicativo.
+            </SheetPrimitive.Description>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
