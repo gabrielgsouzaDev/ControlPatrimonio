f0f4da197a34b471d8144b57a9b1c4ebbddf51f1
Gabriel Gonçalves de Souza <dielgdesouza@gmail.com>
2025-10-14 13:00:09 +0000

    acho que não está responsivo o suficiente para telas pequenas...
    princi

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -114,7 +114,7 @@ export default function DashboardPage() {
   return (
     <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
       <h2 className="text-3xl font-headline tracking-tight">Dashboard</h2>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 grid-cols-2 lg:grid-cols-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total de Itens</CardTitle>
@@ -145,11 +145,9 @@ export default function DashboardPage() {
             <p className="text-xs text-muted-foreground">Cidades com patrimônio alocado.</p>
           </CardContent>
         </Card>
-      </div>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Itens Criados (Último Mês)</CardTitle>
+            <CardTitle className="text-sm font-medium">Itens Criados (Mês)</CardTitle>
             <PlusSquare className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
@@ -159,7 +157,7 @@ export default function DashboardPage() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Itens Atualizados (Último Mês)</CardTitle>
+            <CardTitle className="text-sm font-medium">Itens Atualizados (Mês)</CardTitle>
             <PenSquare className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
@@ -169,7 +167,7 @@ export default function DashboardPage() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Itens Excluídos (Último Mês)</CardTitle>
+            <CardTitle className="text-sm font-medium">Itens Excluídos (Mês)</CardTitle>
             <MinusSquare className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
@@ -237,5 +235,3 @@ export default function DashboardPage() {
     </div>
   );
 }
-
-    
